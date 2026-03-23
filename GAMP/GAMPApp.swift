4af2cab8bf5809f83993167718b165c06742ef33
Aleksander Marciniak <apkisix123@gmail.com>
2026-03-23 18:12:55 +0100

    Initial Commit

---
+++ GAMP/GAMPApp.swift
@@ -0,0 +1,17 @@
+//
+//  GAMPApp.swift
+//  GAMP
+//
+//  Created by Aleksander Marciniak on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct GAMPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
